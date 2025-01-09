ec525d0eaad86db2898c845174ec8ddee9ab2185
jonathancaleb <jonathancaleb230@gmail.com>
2025-01-09 19:08:27 +0300

    refactor main.go: simplify code by removing SaveData1 function and adding main function

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
-import "os"
+import (
+	"fmt"
+)
 
-func SaveData1(path string, data []byte) error {
-	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
-	if err != nil {
-		return err
-	}
-	defer fp.Close()
-
-	_, err = fp.Write(data)
-	if err != nil {
-		return err
-	}
-	return fp.Sync() // fsync
+func main() {
+	fmt.Println("Hello World")
 }
