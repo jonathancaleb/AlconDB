d77f02006e79292e94df3305668aadb1b62055c3
jonathancaleb <jonathancaleb230@gmail.com>
2025-01-18 09:59:56 +0300

    add SaveData1 function for file writing and demonstrate usage in main

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"os"
 )
 
+func SaveData1(path string, data []byte) error {
+	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	_, err = fp.Write(data)
+	if err != nil {
+		return err
+	}
+	return fp.Sync() // fsync
+}
+
 func main() {
-	fmt.Println("Hello World")
+	// Example usage of SaveData1
+	err := SaveData1("example.txt", []byte("Hello, World!"))
+	if err != nil {
+		panic(err)
+	}
 }
