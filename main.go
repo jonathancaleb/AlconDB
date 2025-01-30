10a7147d4c61318d6e7d599733818795ef131d56
jonathancaleb <jonathancaleb230@gmail.com>
2025-01-30 10:29:59 +0300

    implement SaveDataImproved function for enhanced file writing with context support

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"math/rand"
 	"os"
+	"path/filepath"
 )
 
 // SaveData1 writes data directly to the file at the given path.
@@ -59,16 +65,56 @@ func SaveData3(path string, data []byte) error {
 	return os.Rename(tmp, path)
 }
 
+func randomInt() int {
+	var b [8]byte
+	_, err := rand.Read(b[:])
+	if err != nil {
+		panic(err) // Handle error appropriately
+	}
+	return int(binary.LittleEndian.Uint64(b[:]))
+}
+
+func SaveDataImproved(ctx context.Context, path string, data []byte) error {
+	dir := filepath.Dir(path)
+	tmp := filepath.Join(dir, fmt.Sprintf(".%s.tmp.%d", filepath.Base(path), randomInt()))
+	log.Printf("Writing data to temporary file: %s", tmp)
+
+	fp, err := os.OpenFile(tmp, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
+	if err != nil {
+		if errors.Is(err, os.ErrPermission) {
+			return fmt.Errorf("permission denied: %w", err)
+		}
+		return err
+	}
+	defer fp.Close()
+	defer os.Remove(tmp)
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err() // Return if context is canceled
+	default:
+	}
+
+	if _, err := io.WriteString(fp, string(data)); err != nil {
+		return err
+	}
+	if err := fp.Sync(); err != nil {
+		return err
+	}
+
+	log.Printf("Renaming temporary file to: %s", path)
+	return os.Rename(tmp, path)
+}
+
 func main() {
-	// Example usage of SaveData2
 	path := "example.txt"
 	data := []byte("This is some example data.")
 
-	err := SaveData3(path, data)
+	err := SaveDataImproved(context.Background(), path, data)
 	if err != nil {
-		fmt.Printf("Error saving data: %v\n", err)
+		log.Printf("Error saving data: %v\n", err)
 		return
 	}
 
-	fmt.Println("Data saved atomically!")
+	log.Println("Data saved atomically!")
 }
